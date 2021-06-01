25dc80c7ef5fb35255d643aac07c318c5b42d983
Martin Levesque <levesque.martin@gmail.com>
2021-06-01 08:19:02 -0400

    reinit www

---
+++ www-cronrobot/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root :to => 'home#index'
+
+  get '/', to: 'home#index'
+
+  namespace :dashboard do
+    get '/', to: 'home#index'
+    get '/schedulers', to: 'schedulers#index'
+  end
+
+end
