6278aa46978f9f194af12e09d8893f130bd904fc
Martin Levesque <levesque.martin@gmail.com>
2021-06-02 21:25:11 -0400

    debug

--- tests/e2e/api_resources.sh
+++ tests/e2e/api_resources.sh
@@ -6,6 +6,9 @@ resource_name="resource11"
 to_create='{"name": "'"$resource_name"'", "reference_id": "11233", "data": "MYDATA"}'
 result_create=$(curl -X POST -H "Content-Type: application/json" -d "$to_create" $API_BASE_URL/resources/)
 
+echo "res -->"
+echo $result_create
+
 created_resource_id=$(echo "$result_create" | jq .id)
 
 # GET it
