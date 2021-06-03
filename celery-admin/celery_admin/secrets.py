bdecca6136459a0e8dc5482047eb040db192764c
Martin Levesque <levesque.martin@gmail.com>
2021-06-02 21:11:19 -0400

    fixes

--- celery-admin/celery_admin/secrets.py
+++ celery-admin/celery_admin/secrets.py
@@ -5,19 +5,24 @@ secret_type = os.environ.get("SECRET_TYPE", "FERNET")
 
 SUPPORTED_SECRET_TYPES = ["FERNET"]
 
+
 def get_secret_key():
     return os.environ.get("FERNET_SECRET_KEY", "")
 
+
 def decrypt(encrypted_msg):
     return decrypt_fernet(encrypted_msg)
+
 
 def decrypt_fernet(encrypted_msg):
     f = Fernet(get_secret_key())
 
     return f.decrypt(encrypted_msg.encode()).decode("utf-8")
 
+
 def encrypt(msg):
     return encrypt_fernet(msg)
+
 
 def encrypt_fernet(msg):
     f = Fernet(get_secret_key())
