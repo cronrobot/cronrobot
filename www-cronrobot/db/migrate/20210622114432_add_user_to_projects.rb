3510ae3bf4b89022177bf401534f495f1fe4059f
Martin Levesque <levesque.martin@gmail.com>
2021-06-22 07:56:30 -0400

    cleanup

--- www-cronrobot/db/migrate/20210622114432_add_user_to_projects.rb
+++ www-cronrobot/db/migrate/20210622114432_add_user_to_projects.rb
@@ -1,6 +1,5 @@
 class AddUserToProjects < ActiveRecord::Migration[7.0]
   def change
-    #add_column :projects, :user_id, :integer
     add_reference :projects, :user, foreign_key: true
   end
 end
