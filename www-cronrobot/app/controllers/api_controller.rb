8b05b60feca488f9ed4aa84e8a7dc7b307404b56
Martin Lévesque <levesque.martin@gmail.com>
2021-08-01 10:39:20 -0400

    Resources routes should not html entities escape #144 (#146)

--- www-cronrobot/app/controllers/api_controller.rb
+++ www-cronrobot/app/controllers/api_controller.rb
@@ -1,6 +1,10 @@
 class ApiController < ApplicationController
   include Secured
   
-  private
+  protected
+
+  def disable_json_escaping
+    ActiveSupport.escape_html_entities_in_json = false
+  end
 
 end
--- www-cronrobot/test/controllers/api/resource_controller_test.rb
+++ www-cronrobot/test/controllers/api/resource_controller_test.rb
@@ -43,4 +43,34 @@ class ResourceControllerTest < ActionDispatch::IntegrationTest
     assert response.parsed_body[0]["params"]["name"] == "VAR"
     assert response.parsed_body[0]["params"]["value"] == "v1"
   end
+
+  test '/api/projects/project_id/resources/type with special characters' do
+    p = Project.last
+    p2 = Project.first
+
+    assert p != p2
+
+    Resource.create!(
+      type: "ResourceProject",
+      reference_id: p2.id,
+      params: {"name" => "VAR2", "value" => "v1"},
+      sub_type: "ResourceProjectVariable"
+    )
+
+    resource = Resource.create!(
+      type: "ResourceProject",
+      reference_id: p.id,
+      params: {"name" => "VAR && nice", "value" => "v1 && yes"},
+      sub_type: "ResourceProjectVariable"
+    )
+
+    get "/api/projects/#{p.id}/resources/ResourceProjectVariable",
+      headers: api_headers,
+      as: :json
+
+    puts "#{response.parsed_body.inspect}"
+    assert response.parsed_body.count == 1
+    assert response.parsed_body[0]["params"]["name"] == "VAR && nice"
+    assert response.parsed_body[0]["params"]["value"] == "v1 && yes"
+  end
 end
