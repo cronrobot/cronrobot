f9d58b4e0ac02e10ae118ca35a8739daf6df9c74
Martin Levesque <levesque.martin@gmail.com>
2021-08-02 18:10:45 -0400

    wip

--- www-cronrobot/app/javascript/packs/application.js
+++ www-cronrobot/app/javascript/packs/application.js
@@ -15,7 +15,7 @@ import "bootstrap"
 // import "@fortawesome/fontawesome-free/css/all"
 import "@fortawesome/fontawesome-free/js/all";
 
-import "../stylesheets/application.css"
+import "../stylesheets/application"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
