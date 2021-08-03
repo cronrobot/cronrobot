d3c4818b332b20a205a17384c8705cc04f14b972
Martin Levesque <levesque.martin@gmail.com>
2021-08-03 08:47:57 -0400

    Split admin, application stylesheets #155

---
+++ www-cronrobot/app/javascript/packs/dashboard.js
@@ -0,0 +1,36 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import '../stylesheets/dashboard'
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+// bootstrap
+import "bootstrap"
+
+// https://dev.to/yarotheslav/how-to-install-fontawesome-with-yarn-and-webpacker-in-rails-6-2k62
+// import "@fortawesome/fontawesome-free/css/all"
+
+//import "../stylesheets/application"
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+window.formHide = function formHide(id) {
+    const customScheduleBlock = document.getElementById(id)
+    customScheduleBlock.style.display = 'none'
+}
+
+window.formShow = function formHide(id) {
+    const customScheduleBlock = document.getElementById(id)
+    customScheduleBlock.style.display = ''
+}
