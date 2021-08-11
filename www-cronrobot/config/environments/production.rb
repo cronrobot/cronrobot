ee2de36129f2601409e350d7ab89812eee607bc8
Martin Levesque <levesque.martin@gmail.com>
2021-08-11 09:01:20 -0400

    wip

--- www-cronrobot/config/environments/production.rb
+++ www-cronrobot/config/environments/production.rb
@@ -109,4 +109,10 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: Rails.application.credentials.dig(:mailgun, :api_key),
+    domain: Rails.application.credentials.dig(:mailgun, :domain)
+  }
 end
