5e2e86bcc9e8a678ab4e98a05e461a054c0bb9bc
Martin Levesque <levesque.martin@gmail.com>
2021-08-29 10:05:13 -0400

    improve first boot

---
+++ init_env.sh
@@ -0,0 +1,9 @@
+
+echo "Ensure .env file (For celery-admin)"
+touch .env
+
+echo "Ensure .grafana.env file (For Grafana)"
+touch .grafana.env
+
+echo "Ensure www-cronrobot/.env file (For www-api)"
+touch www-cronrobot/.env
