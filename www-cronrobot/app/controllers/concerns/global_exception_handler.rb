089aeffee2dad4a985df26949bf0ccb7ec4efebc
Martin Lévesque <levesque.martin@gmail.com>
2021-09-13 21:12:11 -0400

    some improvements (#260)

--- www-cronrobot/app/controllers/concerns/global_exception_handler.rb
+++ www-cronrobot/app/controllers/concerns/global_exception_handler.rb
@@ -5,11 +5,15 @@ module GlobalExceptionHandler
   included do
     rescue_from StandardError do |e|
       flash[:error] = "Error: #{e}"
+      Rails.logger.error e.message
+      Rails.logger.error e.backtrace.join "\n"
       redirect_back fallback_location: root_path
     end
 
     rescue_from Exception do |e|
       flash[:error] = "Error: #{e}"
+      Rails.logger.error e.message
+      Rails.logger.error e.backtrace.join "\n"
       redirect_back fallback_location: root_path
     end
 
