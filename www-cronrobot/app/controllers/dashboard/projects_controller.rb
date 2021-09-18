ba830b6fa7350d08d794d42a707794d8b27c1b46
Martin Lévesque <levesque.martin@gmail.com>
2021-09-18 10:13:21 -0400

    manage project (#271)

--- www-cronrobot/app/controllers/dashboard/projects_controller.rb
+++ www-cronrobot/app/controllers/dashboard/projects_controller.rb
@@ -1,7 +1,19 @@
 class Dashboard::ProjectsController < DashboardController
 
-  def set_current_section
-    @section = "project"
+  def index
+    @projects = @current_user.projects
+  end
+
+  def show
+    @updated_project = current_project
+  end
+
+  def update
+    project = current_project
+    project.update!(allowed_params)
+
+    flash[:success] = "Project updated successfully!"
+    redirect_to controller: :projects, action: :index
   end
 
   def new
@@ -14,13 +26,30 @@ class Dashboard::ProjectsController < DashboardController
     Project.create!(allowed_params.merge(user_id: @current_user.id))
 
     flash[:success] = "Project created successfully!"
-    redirect_to controller: :home, action: :index
+    redirect_to controller: :projects, action: :index
+  end
+
+  def destroy
+
+    project = current_project
+    project.destroy!
+
+    flash[:success] = "Project successfully removed!"
+    redirect_to controller: :projects, action: :index
   end
 
   protected
 
+  def current_project
+    @current_user.projects.find { |p| p.id == params["id"].to_i }
+  end
+
   def allowed_params
     params.require(:project).permit(:name)
+  end
+
+  def set_current_section
+    @section = "project"
   end
 
 end
